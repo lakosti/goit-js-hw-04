7d6e6d6eb48ca6bbe188f2a965435f1579a3685c
Yaroslava <yaroslava.hovenko@gmail.com>
2023-11-09 23:05:09 +0200

    find the better solution

--- js/task-1.js
+++ js/task-1.js
@@ -6,7 +6,7 @@ const isEnoughCapacity = function (products, containerSize) {
   for (const key in products) {
     total += products[key];
   }
-  return total > containerSize ? false : true;
+  return total < containerSize;
 };
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
 console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
